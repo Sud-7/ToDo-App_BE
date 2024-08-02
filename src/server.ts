110a25648b777b6d4568032281b80c964b395cee
Sud <155895839+Sud-7@users.noreply.github.com>
2024-08-02 20:15:33 +0530

    Added Environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 5000;
@@ -8,9 +11,7 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://sjharock99:xo9E98nwIMNo33dL@cluster0.txr1ux5.mongodb.net/checking-Typescript"
-);
+mongoose.connect(process.env.MONGO_URL || "");
 
 interface Task extends mongoose.Document {
   title: string;
